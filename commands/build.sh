063843e550ac3ae91da9ce028e5cb54e13afdc9e
aliooo36 <izhan1642@gmail.com>
2025-11-30 15:44:27 -0500

    appended student names

--- commands/build.sh
+++ commands/build.sh
@@ -1,4 +1,4 @@
 echo -e "1,A\n2,B\n3,C\n4,D\n5,E" > ../inputs/rubric/rubric.txt
 cd ../code
-g++ parta.cpp -o parta
-g++ partb.cpp -o partb
+g++ parta_101291890_101303925.cpp -o parta
+g++ partb_101291890_101303925.cpp -o partb
